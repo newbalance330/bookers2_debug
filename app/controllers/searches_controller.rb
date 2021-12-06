70daa5f3bc8644bf27fd6000725314e044380277
newbalance330 <92163228+newbalance330@users.noreply.github.com>
2021-12-06 08:13:15 +0000

    [Finish]応用課題5

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -8,6 +8,7 @@ class SearchesController < ApplicationController
 
   def search
     @range = params[:range]
+    @word = params[:word]
 
     if @range == "User"
       @users = User.looks(params[:search], params[:word])

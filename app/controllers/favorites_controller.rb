b29a6e361915787a310052770d26375ed2234a3f
newbalance330 <92163228+newbalance330@users.noreply.github.com>
2021-12-08 05:30:25 +0000

    [Add]いいね非同期

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,18 +1,18 @@
 class FavoritesController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.new(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.new(book_id: @book.id)
     favorite.save
-    redirect_to request.referer
+    # redirect_to request.referer 非同期通信伴い必要なくなる
     # 同じページをリダイレクトするコード
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.find_by(book_id: @book.id)
     favorite.destroy
-    redirect_to request.referer
+    # redirect_to request.referer 非同期通信伴い必要なくなる
     # 同じページをリダイレクトするコード
   end
 
